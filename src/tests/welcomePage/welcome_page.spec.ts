d6389258ce33fbc44cea9e36762fc35d94c168ef
Chandrashekar853 <chandrushekar853@gmail.com>
2024-07-25 11:27:04 +0530

    changed daily run cron time

--- src/tests/welcomePage/welcome_page.spec.ts
+++ src/tests/welcomePage/welcome_page.spec.ts
@@ -1,10 +1,10 @@
 // import {test, expect} from "@playwright/test";
-// import LoginPage from "../../pages/Login.page";
+// import LoginPage from "../../pages/SauceDemo/Login.page";
 // import ProductsPage from "../../pages/productsPage.page";
 
 // test("verification login", async ({page})=>{
 //     const loginPageObj = new LoginPage(page);
-//     await loginPageObj.openApplication();
+//     await loginPageObj.navigateToLoginPage();
 //     await loginPageObj.logIn('standard_user', 'secret_sauce')
 //     const  productPageObj = new ProductsPage(page);
 //     await expect(productPageObj.productHeading).toHaveText('Products')
