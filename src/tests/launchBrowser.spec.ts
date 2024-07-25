f9e179ee2926e63d5c7285cb4ea59b2866e1af09
Chandrashekar853 <chandrushekar853@gmail.com>
2024-07-25 10:50:48 +0530

    uncommented launchBrowser spec file

--- src/tests/launchBrowser.spec.ts
+++ src/tests/launchBrowser.spec.ts
@@ -1,20 +1,20 @@
-// import { test,  chromium } from "@playwright/test";
+import { test,  chromium } from "@playwright/test";
 
 
 
-// test.describe('launch', () => {
-//     test('Open browser', async() =>{
-//         const browser = await chromium.launch({
-//             headless: false
-//         })
-//         const context = await browser.newContext()
-//         const page = await context.newPage();
-//         await page.goto('https://letcode.in/')
-//         await browser.close()
-//     })
+test.describe('launch', () => {
+    test('Open browser', async() =>{
+        const browser = await chromium.launch({
+            headless: false
+        })
+        const context = await browser.newContext()
+        const page = await context.newPage();
+        await page.goto('https://letcode.in/')
+        await browser.close()
+    })
     
-// });
+});
 
-// test('launch chrome',async ({page})=>{
-//     await page.goto("https://www.google.com")
-// })
+test('launch chrome',async ({page})=>{
+    await page.goto("https://www.google.com")
+})
